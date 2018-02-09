f4b9f3850a1ae84684c9809f23f79d8e245ef555
Divine <cheldya.obhat@jjay.cuny.edu>
2018-02-08 21:28:14 -0600

    Print int size

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,29 @@
+//
+// Created by Divine on 2/8/2018.
+//
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    std::cout<<"Hi My name is Divine! Hello World"<<endl;
-    return 0;
+    int n;
+    //std::cout<<"please enter an integer from 0 to 10"<<std::endl;
+    cin>>n;
+
+    int x;
+
+    for(x=n/2; x<n; x++)
+    {
+        if(x>=n/2.0 and x<n)
+        {
+            cout<<x;
+            if(x!=n-1)
+            {
+                cout<<",";
+            }
+            ;
+        }
+    }
+    //return 0;
 }
+
