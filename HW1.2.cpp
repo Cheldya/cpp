0b1d461aaca540bddfda69868ed8f285f8a74fa1
Divine <cheldya.obhat@jjay.cuny.edu>
2018-02-08 21:42:23 -0600

    Print each size

---
+++ HW1.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // display the size of each type
+
+    std::cout<<sizeof(int)<<endl;
+    std::cout<<sizeof(short)<<endl;
+    std::cout<<sizeof(float)<<endl;
+    std::cout<<sizeof(double)<<endl;
+    std::cout<<sizeof(long)<<endl;
+    std::cout<<sizeof(long long)<<endl;
+    std::cout<<sizeof(unsigned long)<<endl;
+    std::cout << "Hello, World!" <<endl;
+    return 0;
+}
